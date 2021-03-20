f4f85ab9a6700de986893a798f351ebca4d64dac
Aryan-hab <aryanabdollahi.aa@gmail.com>
2021-03-21 01:45:23 +0330

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,4 @@
 # python_datetime
 calculating time passed (time ago) since something is posted (e.g., on your website, application, etc..)
-\n towards datetime module in python
-\n I'm using this function inside my django project in comments model
+towards datetime module in python
+I'm using this function inside my django project in comments model
